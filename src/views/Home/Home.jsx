733ade6b118814f8d6c7df76ada6f9a92e3f54b4
agustinibar <agustinibarperrotta@gmail.com>
2024-05-30 12:55:56 -0300

    contact base

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import styles from './home.module.css';
 import  Banner  from '../../Scenes/Banner/Banner';
 import Courses from '../../Scenes/Courses/Courses';
+import Contact from '../../Scenes/Contact/Contact';
 
 const Home = () => {
   return (
     <div className={styles.containerHome}>
         <Banner/>
         <Courses/>
+        <Contact/>
     </div>
   )
 }
