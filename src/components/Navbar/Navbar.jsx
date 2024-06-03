a1e3fdfb403e5f67d0224e29a523fd6c2048f447
agustinibar <agustinibarperrotta@gmail.com>
2024-06-03 16:49:52 -0300

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import styles from './navbar.module.css';
+import techlogo from '../../assets/logoTech.jpg';
 
 const Navbar = () => {
   return (
-    <div  className={styles.containerNavbar}>
-        Navbar
+    <>
+    <div  className={styles.navbarContainer}>
+        <img src={techlogo} alt="techlogo" />
+        <p>Inicio</p>
+        <p>Cursos</p>
+        <p>Estudiantes</p>
+        <p>Contacto</p>
+        <button className={styles.navbarButton}> Inscribete ahora!</button>
     </div>
+    </>
   )
 }
 
