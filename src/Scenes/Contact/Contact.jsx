222553dc5625557132037b40a417d70154976d10
agustinibar <agustinibarperrotta@gmail.com>
2024-06-08 13:02:49 -0300

    contact div

--- src/Scenes/Contact/Contact.jsx
+++ src/Scenes/Contact/Contact.jsx
@@ -1,8 +1,30 @@
 import styles from './contact.module.css';
+import logo from '../../assets/logoTech.jpg';
 
 const Contact = () => {
   return (
-    <div className={styles.contact}>Contact</div>
+    <div className={styles.contactContainer}>
+      <div className={styles.contactInfo}>
+          <img src={logo} alt="logotype" />
+          <div className={styles.contact}>
+            <h3>Contacto</h3>
+            <p>62123151</p>
+            <p>adeadadea@ffa.com</p>
+            <p>La calle, 454</p>
+          </div>
+          <div className={styles.socialMedia}>
+            <h3>Redes Sociales</h3>
+            <p>Facebook</p>
+            <p>Twitter</p>
+            <p>Instagram</p>
+            <p>Youtube</p>
+          </div>
+      </div>
+      <div className={styles.reviews}>
+        <h2>Construyamos el mañana</h2>
+      </div>
+      
+    </div>
   )
 }
 
